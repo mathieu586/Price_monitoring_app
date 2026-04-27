efb5e15820c1a7d7f98178dfabc0ddbc54af3526
mathieu586 <mateusz.non@gmai.com>
2026-04-27 20:32:44 +0200

    zzzz

--- Lab6/reporter.py
+++ Lab6/reporter.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 
 from Lab6.validator import SeriesValidator, OutlierDetector, ZeroSpikeDetector, ThresholdDetector
-from zad1 import TimeSeries
+from time_series import TimeSeries
 import statistics
 import validator
 
@@ -15,13 +15,13 @@ class SimpleReporter:
         if not values:
             return [f"Brak danych o pomiarach"]
 
-        return [f"Info: {series.nazwa_wsk}, at {series.kod_stacji} has mean = {statistics.mean(values)}"]
+        return [f"Info: {series.wskaznik}, at {series.kod} has mean = {statistics.mean(values)}"]
 
 
 if __name__ == "__main__":
-    time_series = TimeSeries("wsk1", 2, 3, [datetime(1, 2, 3), datetime(1, 2, 4), datetime(1, 2, 5)], [1, 3, None], "g / m")
+    time_series = TimeSeries("PM10", 2, 33, [datetime(1, 2, 3), datetime(1, 2, 4), datetime(1, 2, 5), datetime(1, 2, 3), datetime(1, 2, 4), datetime(1, 2, 5), datetime(1, 2, 3), datetime(1, 2, 4)], [1, 3, None, 0, None, 0, 200, 12], "g/m3")
 
-    validators = [OutlierDetector(2), ZeroSpikeDetector(), ThresholdDetector(3), SimpleReporter()]
+    validators = [OutlierDetector(2), ZeroSpikeDetector(), ThresholdDetector(10), SimpleReporter()]
 
     for validator in validators:
         res = validator.analyze(time_series)
--- Lab6/validator.py
+++ Lab6/validator.py
@@ -31,7 +31,7 @@ class OutlierDetector(SeriesValidator):
         i = 0
         for wartosc in series.wartosci:
             if wartosc is not None and abs(wartosc - mean) > self.k * stddev:
-                alerts.append(f"Pomiar z dnia {series.daty[i]}, o wartości: {wartosc} przekroczył różnice {self.k} * odchylenie od średniej: {mean}")
+                alerts.append(f"[Outlier detector]Pomiar z dnia {series.daty[i]}, o wartości: {wartosc} przekroczył różnice {self.k} * odchylenie od średniej: {mean}")
 
             i += 1
 
@@ -56,14 +56,14 @@ class ZeroSpikeDetector(SeriesValidator):
             else:
                 if f:
                     f = False
-                    alerts.append(f"Wykryto przynajmniej 3 zera lub 3 braki danych z rzędu, data ostatniego pomiaru z brakiem danych: {series.daty[last_missing_data_index]}")
+                    alerts.append(f"[Zero spike detector]Wykryto przynajmniej 3 zera lub 3 braki danych z rzędu, data ostatniego pomiaru z brakiem danych: {series.daty[last_missing_data_index]}")
 
                 no_data_counter = 0
 
             i += 1
 
         if f:
-            alerts.append(f"Wykryto przynajmniej 3 zera lub 3 braki danych z rzędu, data ostatniego pomiaru z brakiem danych: {series.daty[last_missing_data_index]}")
+            alerts.append(f"[Zero spike detector]Wykryto przynajmniej 3 zera lub 3 braki danych z rzędu, data ostatniego pomiaru z brakiem danych: {series.daty[last_missing_data_index]}")
 
         return alerts
 
@@ -78,13 +78,24 @@ class ThresholdDetector(SeriesValidator):
         i = 0
         for wartosc in series.wartosci:
             if wartosc is not None and wartosc > self.threshold:
-                alerts.append(f"Wykryto pomiar przekraczający zadany próg: {self.threshold}, wartość pomiaru: {wartosc}, data: {series.daty[i]}")
+                alerts.append(f"[Threshold detector]Wykryto pomiar przekraczający zadany próg: {self.threshold}, wartość pomiaru: {wartosc}, data: {series.daty[i]}")
 
             i += 1
 
         return alerts
 
 if __name__ == "__main__":
-    detector = OutlierDetector(2)
+    time_series = TimeSeries("PM10", 2, 33, [datetime(1, 2, 3), datetime(1, 2, 4), datetime(1, 2, 5), datetime(1, 2, 3), datetime(1, 2, 4), datetime(1, 2, 5), datetime(1, 2, 3), datetime(1, 2, 4)], [1, 3, None, 0, None, 0, 200, 12], "g/m3")
+    time_series2 = TimeSeries("PM10", 3, 13, [datetime(1, 2, 4), datetime(1, 2, 5), datetime(1, 2, 3)], [1, 6, 10], "g/m3")
 
-    print(detector.analyze(TimeSeries("asd", 2, 3, [datetime(1, 2, 3), datetime(1, 2, 4), datetime(1, 2, 5)], [1, 3, None], "s")))
+    detector1 = OutlierDetector(2)
+    detector2 = ZeroSpikeDetector()
+    detector3 = ThresholdDetector(10)
+
+    print(detector1.analyze(time_series))
+    print(detector2.analyze(time_series))
+    print(detector3.analyze(time_series))
+
+    print(detector1.analyze(time_series2))
+    print(detector2.analyze(time_series2))
+    print(detector3.analyze(time_series2))
